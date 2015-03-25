f794d467e3b8f00b59d62ec14ea988b63be66adc
zxahu <75340232@qq.com>
2015-03-25 15:48:57 +0800

    1. change logger to singleton model

--- Middle/SysLog.py
+++ Middle/SysLog.py
@@ -1,28 +1,33 @@
 __author__ = 'zhang11'
 
 import logging.handlers
+import os
 
 class SysLogger(object):
 
+    __instance = None
     logger = None
-
     log_level = logging.DEBUG
-    log_file = "/etc/QGA/consumer.log"
+    log_file = "/var/log/QGA/consumer.log"
     format = "%(asctime)s - %(filename)s:%(lineno)s  - %(levelname)s - %(message)s"
 
-    @staticmethod
-    def getLogger(self):
-        if SysLogger.logger is  None:
-            return SysLogger.logger
+    def __new__(cls, *args, **kwargs):
+        if (cls.__instance is None):
+            cls.__instance = super(SysLogger, cls).__new__(cls, *args, **kwargs)
+        return cls.__instance
 
-        SysLogger.logger = logging.getLogger()
-        log_handler = logging.handlers.BaseRotatingHandler(filename= SysLogger.log_file)
-        log_fmt = logging.Formatter(SysLogger.format)
+    def __init__(self):
+        self.test_path()
+        self.logger = logging.getLogger()
+        log_handler = logging.handlers.RotatingFileHandler(self.log_file, maxBytes = 1024*1024, backupCount = 5)
+        log_fmt = logging.Formatter(self.format)
         log_handler.setFormatter(log_fmt)
-        SysLogger.logger.addHandler(log_handler)
-        SysLogger.logger.setLevel(SysLogger.log_level)
-        return SysLogger
+        self.logger.addHandler(log_handler)
+        self.logger.setLevel(self.log_level)
 
+    def test_path(self):
+        if not os.path.exists("/var/log/QGA/"):
+            os.makedirs("/var/log/QGA/")
 
 
 
